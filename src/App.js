7798d1cc4707d34b292b4241d3ed8beb9f2d149c
steve-carpenter <carpenter.steven1@gmail.com>
2021-05-26 17:09:18 -0400

    updating to show CUP info

--- src/App.js
+++ src/App.js
@@ -4,26 +4,60 @@ import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
+import ReactJson from 'react-json-view';
 Amplify.configure(aws_exports);
 
 class App extends Component {
+
+  componentDidMount() {
+    this.getUserInfo();
+    this.getSessionInfo();
+    this.getUserCreds();
+    }
+
+state = {}
+
+getUserInfo(){
+  console.log(Auth.currentAuthenticatedUser());
+  Auth.currentAuthenticatedUser().then((res)=>{
+    console.log(res);
+    this.setStateAsync({user: res.username});
+  });
+}
+
+getSessionInfo(){
+  console.log(Auth.currentSession());
+  Auth.currentSession().then((res)=>{
+    console.log(res);
+    this.setStateAsync({token: res.getIdToken().getJwtToken(),
+    expiration: new Intl.DateTimeFormat("en-US").format(res.getIdToken().getExpiration()),
+  response: res});
+  });
+}
+
+getUserCreds(){
+  console.log(Auth.currentSession());
+  Auth.currentUserCredentials().then((res)=>{
+    console.log(res);
+  });
+}
+
+setStateAsync(state) {
+  return new Promise((resolve) => {
+    this.setState(state, resolve)
+  });
+}
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload.{this.state.user} {this.state.expiration} 
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <ReactJson src={this.state.response} />
       </div>
     );
   }
